7664ad03022d8b49c866a8cd33cc65e553e4bcd7
edrysse <117366780+edrysse@users.noreply.github.com>
2024-10-30 02:59:13 +0100

    managing files

--- resources/views/client/command.blade.php
+++ resources/views/client/command.blade.php
@@ -2,30 +2,32 @@
 @section('content')
     @include('client.includes.aside')
     <base href="/public">
-    <section class="bg-title-page flex-c-m p-t-160 p-b-80 p-l-15 p-r-15"
-        style="background-image: url(clientpage/images/bg-title-page-02.jpg);">
-        <div style="    display: flex;
-            flex-direction: column;
-            align-items: center;">
-                <h2 class="tit6 t-center">
-                    Commande
-                    
-                </h2>
-        
-                <div class="mb-4" >
-                    {{-- Merci Laayoune --}}
-                    <img class="mercilogo-autre"  src="clientpage/images/MERCI_IMG/LOGO/Logo-Merci-b1.png" alt="" >
-                </div>
-                
-                <div style="    display: flex;
-                align-items: center;">
-                    <a href="https://www.facebook.com/mercilaayoune"><img src="clientpage/images/MERCI_IMG/social-media-merci/facebook-app-symbol-merci.png" alt="" width="22px"></a>
-                    <a href="https://www.instagram.com/mercilaayoune1"><img class="ml-2" src="clientpage/images/MERCI_IMG/social-media-merci/instagram-merci.png" alt="" width="22px"></a>
-                    <a href="https://www.tiktok.com/@mercilaayoune"><img class="ml-2" src="clientpage/images/MERCI_IMG/social-media-merci/tik-tok-merci.png" alt="" width="22px"></a>
-                    <a href=""><img class="ml-2" src="clientpage/images/MERCI_IMG/social-media-merci/snapchat.png" alt="" width="22px"></a>
-                    <a href="https://shorturl.at/cnrt1"><img class="ml-2" src="clientpage/images/MERCI_IMG/social-media-merci/pin-merci.png" alt="" width="22px"></a>
-                </div>
-            
+    <section class="bg-title-page flex-c-m p-t-160 p-b-80 p-l-15 p-r-15" style="background-image: url({{ secure_url('clientpage/images/bg-title-page-02.jpg') }});">
+        <div style="display: flex; flex-direction: column; align-items: center;">
+            <h2 class="tit6 t-center">
+                Commande
+            </h2>
+            <div class="mb-4">
+                {{-- Merci Laayoune --}}
+                <img class="mercilogo-autre" src="{{ secure_url('clientpage/images/MERCI_IMG/LOGO/Logo-Merci-b1.png') }}" alt="">
+            </div>
+            <div style="display: flex; align-items: center;">
+                <a href="https://www.facebook.com/mercilaayoune">
+                    <img src="{{ secure_url('clientpage/images/MERCI_IMG/social-media-merci/facebook-app-symbol-merci.png') }}" alt="" width="22px">
+                </a>
+                <a href="https://www.instagram.com/mercilaayoune1">
+                    <img class="ml-2" src="{{ secure_url('clientpage/images/MERCI_IMG/social-media-merci/instagram-merci.png') }}" alt="" width="22px">
+                </a>
+                <a href="https://www.tiktok.com/@mercilaayoune">
+                    <img class="ml-2" src="{{ secure_url('clientpage/images/MERCI_IMG/social-media-merci/tik-tok-merci.png') }}" alt="" width="22px">
+                </a>
+                <a href="">
+                    <img class="ml-2" src="{{ secure_url('clientpage/images/MERCI_IMG/social-media-merci/snapchat.png') }}" alt="" width="22px">
+                </a>
+                <a href="https://shorturl.at/cnrt1">
+                    <img class="ml-2" src="{{ secure_url('clientpage/images/MERCI_IMG/social-media-merci/pin-merci.png') }}" alt="" width="22px">
+                </a>
+            </div>
         </div>
     </section>
     <!-- Reservation -->
@@ -37,25 +39,13 @@
                         <span class="tit2 t-center">
                             Commande
                         </span>
-
                         <h3 class="tit3 t-center m-b-35 m-t-2">
                             passer votre commande
                         </h3>
                     </div>
-
-
-
-                    
-                    
-
                     @yield('validation')
-
                 </div>
             </div>
-            
-           
         </div>
-        
     </section>
-    
 @endsection
--- resources/views/client/contact.blade.php
+++ resources/views/client/contact.blade.php
@@ -2,9 +2,9 @@
 
 @section('meta')
 <title>Merci Laayoune - Contact</title>
-<meta name="description" content="Nous sommes là pour vous écouter. Utilisez nos coordonnées pour nous contacter au Merci Laayoune. Que ce soit pour des questions, 
+<meta name="description" content="Nous sommes là pour vous écouter. Utilisez nos coordonnées pour nous contacter au Merci Laayoune. Que ce soit pour des questions,
 des commentaires ou des réservations spéciales, n'hésitez pas à entrer en contact avec notre équipe dévouée.">
-    <meta name="keywords" content="Contacter Merci Laayoune, Coordonnées, Questions, Commentaires, Réservations spéciales, Équipe dévouée."> 
+    <meta name="keywords" content="Contacter Merci Laayoune, Coordonnées, Questions, Commentaires, Réservations spéciales, Équipe dévouée.">
     <meta property="og:locale" content="fr_FR">
     <meta property="og:type" content="website">
     <meta property="og:title" content="Merci Laayoune - Contact">
@@ -24,14 +24,14 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
             align-items: center;">
                 <h2 class="tit6 t-center">
                     Contact
-                    
+
                 </h2>
-        
+
                 <div class="mb-4" >
                     {{-- Merci Laayoune --}}
                     <img class="mercilogo-autre"  src="clientpage/images/MERCI_IMG/LOGO/Logo-Merci-b1.png" alt="" >
                 </div>
-                
+
                 <div style="    display: flex;
                 align-items: center;">
                     <a href="https://www.facebook.com/mercilaayoune"><img src="clientpage/images/MERCI_IMG/social-media-merci/facebook-app-symbol-merci.png" alt="" width="22px"></a>
@@ -40,7 +40,7 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
                     <a href="https://t.snapchat.com/Df0EWYBp"><img class="ml-2" src="clientpage/images/MERCI_IMG/social-media-merci/snapchat.png" alt="" width="22px"></a>
                     <a href="https://shorturl.at/cnrt1"><img class="ml-2" src="clientpage/images/MERCI_IMG/social-media-merci/pin-merci.png" alt="" width="22px"></a>
                 </div>
-            
+
         </div>
     </section>
 
@@ -54,7 +54,7 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
         letter-spacing: 1px
     }
 
-    
+
 
 
 @media (max-width: 992px){
@@ -108,67 +108,53 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
 
     <!-- Contact form -->
     <section class="section-contact bg1-pattern p-t-90">
-        
+
 
         <div class="container">
             <h3 class="tit7 t-center p-b-62 p-t-105">
                 ENVOYEZ-NOUS UN MESSAGE
             </h3>
 
-            <form action="{{ route('createContact') }}" class="wrap-form-reservation size22 m-l-r-auto" method="patch">
-                @csrf
-                <div class="row">
-                    <div class="col-md-4">
-                        <!-- Name -->
-                        <span class="txt9">
-                            Nom complet
-                        </span>
+            <form action="{{ secure_url('createContact') }}" class="wrap-form-reservation size22 m-l-r-auto" method="post">
+    @csrf
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <div class="row">
+        <div class="col-md-4">
+            <span class="txt9">Nom complet</span>
+            <div class="wrap-inputname size12 bo2 bo-rad-10 m-t-3 m-b-23">
+                <input class="bo-rad-10 sizefull txt10 p-l-20" type="text" name="name" placeholder="Nom complet" aria-label="Nom complet" required>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <span class="txt9">Email</span>
+            <div class="wrap-inputemail size12 bo2 bo-rad-10 m-t-3 m-b-23">
+                <input class="bo-rad-10 sizefull txt10 p-l-20" type="email" name="email" placeholder="Email" aria-label="Email" required>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <span class="txt9">Telephone</span>
+            <div class="wrap-inputphone size12 bo2 bo-rad-10 m-t-3 m-b-23">
+                <input class="bo-rad-10 sizefull txt10 p-l-20" type="tel" name="phone" placeholder="Telephone" aria-label="Telephone" required>
+            </div>
+        </div>
+        <div class="col-12">
+            <span class="txt9">Message</span>
+            <textarea class="bo-rad-10 size35 bo2 txt10 p-l-20 p-t-15 m-b-10 m-t-3" name="message" placeholder="Message" aria-label="Message" required></textarea>
+        </div>
+    </div>
+    <div class="wrap-btn-booking flex-c-m m-t-13">
+        <button type="submit" class="btn3 flex-c-m size36 txt11 trans-0-4">envoyer</button>
+    </div>
+</form>
 
-                        <div class="wrap-inputname size12 bo2 bo-rad-10 m-t-3 m-b-23">
-                            <input class="bo-rad-10 sizefull txt10 p-l-20" type="text" name="name" placeholder="Nom complet">
-                        </div>
-                    </div>
-
-                    <div class="col-md-4">
-                        <!-- Email -->
-                        <span class="txt9">
-                            Email
-                        </span>
-
-                        <div class="wrap-inputemail size12 bo2 bo-rad-10 m-t-3 m-b-23">
-                            <input class="bo-rad-10 sizefull txt10 p-l-20" type="text" name="email"
-                                placeholder="Email">
-                        </div>
-                    </div>
-
-                    <div class="col-md-4">
-                        <!-- Phone -->
-                        <span class="txt9">
-                            Telephone
-                        </span>
-
-                        <div class="wrap-inputphone size12 bo2 bo-rad-10 m-t-3 m-b-23">
-                            <input class="bo-rad-10 sizefull txt10 p-l-20" type="text" name="phone"
-                                placeholder="Telephone">
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <!-- Message -->
-                        <span class="txt9">
-                            Message
-                        </span>
-                        <textarea class="bo-rad-10 size35 bo2 txt10 p-l-20 p-t-15 m-b-10 m-t-3" name="message" placeholder="Message"></textarea>
-                    </div>
-                </div>
-
-                <div class="wrap-btn-booking flex-c-m m-t-13">
-                    <!-- Button3 -->
-                    <button type="submit" class="btn3 flex-c-m size36 txt11 trans-0-4">
-                        envoyer
-                    </button>
-                </div>
-            </form>
 
 
 
@@ -189,13 +175,13 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
                                 </span>
                             </div>
                         </div>
-                        
+
                         <div>
                             <span class="txt23 size38" >
                                 <i class="fas fa-map-marker-alt"></i> En face hotel nagjir, Av Es-smara, Laayoune
                             </span>
                         </div>
-                       
+
                     </div>
 
 
@@ -213,7 +199,7 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
                                 </span>
                             </div>
                         </div>
-                        
+
 
                         <div style="    display: flex;
                         flex-direction: column;">
@@ -224,7 +210,7 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
                                 <i class="fa-solid fa-globe mr-1"></i>www.mercilaayoune.com
                             </span>
                         </div>
-                        
+
                     </div>
 
 
@@ -240,7 +226,7 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
                                 </span>
                             </div>
                         </div>
-                        
+
 
                         <span class="txt23 size38">
                             <i class="fas fa-phone mr-1"></i>080-8616369
@@ -252,8 +238,8 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
                     </div>
 
 
-                    
-                   
+
+
 
 
                    <div class = "col-sm-3 col-md-3 col-lg-3 pb-4" >
@@ -268,40 +254,40 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
                                 </span>
                             </div>
                         </div>
-                        
+
 
                         <span class="txt23 size38">
                             <ul class="p-0" style="display:flex">
                                 <li class="txt14 m-b-14 mr-2">
-                                    
+
                                     <div>
                                         <a href="https://www.facebook.com/mercilaayoune" style="color: #666666;"><i class="fab fa-facebook-f fs-16"></i></a>
                                     </div>
-                                    
+
                                 </li>
-            
+
                                 <li class="txt14 m-b-14 mr-2">
-                                    
+
                                     <div>
                                         <a href="https://www.instagram.com/mercilaayoune1" style="color: #666666;"><i class="fab fa-instagram fs-16"></i></a>
                                     </div>
-                                    
+
                                 </li>
-            
+
                                 <li class="txt14 m-b-14 mr-2">
-                                    
+
                                     <div>
                                         <a href="https://www.tiktok.com/@mercilaayoune" style="color: #666666;"><i class="fab fa-tiktok fs-16"></i></a>
                                     </div>
-                                    
+
                                 </li>
-            
+
                                 <li class="txt14 m-b-14 mr-2">
-                                    
+
                                     <div>
                                         <a href="https://t.snapchat.com/Df0EWYBp" style="color: #666666;"><i class="fa-brands fa-snapchat"></i></a>
                                     </div>
-                                    
+
                                 </li>
                             </ul>
                         </span>
@@ -330,7 +316,7 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
 
 
 
-    
+
     <!-- Map -->
     <section class="google-map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3550.1794475737815!2d-13.199997324777996!3d27.150643649825604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc3773a103f1c107%3A0xa1eb7796bf4aba3!2sMerci%20Laayoune!5e0!3m2!1sen!2s!4v1692271441632!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
@@ -361,7 +347,7 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
                     <button class="btn btn-outline-success">Fermer</button>
                 </div>
             </div>
-        
+
         </div>
     </div>
   </div>
@@ -381,7 +367,7 @@ des commentaires ou des réservations spéciales, n'hésitez pas à entrer en co
             });
         </script>';
     @endphp
-    
+
 @endif
 
 
